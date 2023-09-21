61eabde4343fb923be858ea5062f1161a42af488
Luba <l.a.rufai@gmail.com>
2023-09-21 20:40:11 +0000

    currentPosition

--- src/levelup/character.py
+++ src/levelup/character.py
@@ -1,8 +1,12 @@
+from levelup.position import Position
 class Character:
     name = "Autobot"
+    currentPosition = None
 
     def __init__(self, character_name):
         if character_name != "":
             self.name = character_name
 
+    def changePosition(self, new_position):
+        self.currentPosition = new_position
 
--- tests/test_character.py
+++ tests/test_character.py
@@ -1,3 +1,4 @@
+from levelup.position import Position
 from unittest import TestCase
 from levelup.character import Character
 
@@ -11,3 +12,14 @@ class TestCharacterInitWithDefaultName(TestCase):
     def test_character_name(self):
         testobj = Character("")
         self.assertEqual("Autobot", testobj.name)
+
+
+class TestCharacterCurrentPosition(TestCase):
+    def test_character_position(self):
+        TEST_X=4
+        TEST_Y=0
+        test_char = Character("")
+        test_pos = Position(TEST_X, TEST_Y)
+        test_char.changePosition(test_pos)
+        self.assertEqual(test_char.currentPosition.x_coordinate,TEST_X)
+        self.assertEqual(test_char.currentPosition.y_coordinate,TEST_Y)
