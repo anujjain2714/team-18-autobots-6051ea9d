52e307d2bb6aa97c2de61f12c9b124c86fd6e36b
Luba <l.a.rufai@gmail.com>
2023-09-22 14:48:36 +0000

    map draft

--- src/levelup/map.py
+++ src/levelup/map.py
@@ -1,12 +1,25 @@
 from levelup.character import Character
 from levelup.position import Position
+import random
+import array
 
+X_DIMENSION = 10
+Y_DIMENSION = 10
 class Map:
+    startingPosition = None
 
     def __init__(self):
-        pass
+        random_x = random.randint(0, X_DIMENSION-1)
+        random_y = random.randint(0, Y_DIMENSION-1)
+        self.startingPosition = Position(random_x, random_y)
 
     def isPositionValid(self, position):
+        return ((0 <= position.x_coordinate) & (position.x_coordinate < X_DIMENSION) & (0 <= position.y_coordinate) & (position.y_coordinate < Y_DIMENSION))
+
+    def getStartingPosition(self):
+        return self.startingPosition
+
+    def calculateNewPosition(self, direction):
         pass
 
 class DummyMap(Map):
@@ -16,3 +29,6 @@ class DummyMap(Map):
 
     def isPositionValid(self, position):
         return True
+    
+    def getStartingPosition(self):
+        return Position(0,0)
--- tests/test_map.py
+++ tests/test_map.py
@@ -2,3 +2,33 @@ from unittest import TestCase
 from levelup.character import Character
 from levelup.position import Position
 from levelup.map import Map
+from levelup.map import DummyMap
+
+class TestDummyMapInit(TestCase):
+    def test_dummy_map_init(self):
+        test_map = DummyMap()
+        test_position = test_map.getStartingPosition()
+        self.assertTrue(test_map.isPositionValid(test_position))
+
+class TestMapInit(TestCase):
+    def test_map_init(self):
+        test_map = Map()
+        test_position = test_map.getStartingPosition()
+        print("Test Position is X:" + str(test_position.x_coordinate) + " Y: " + str(test_position.y_coordinate))
+        self.assertTrue(test_map.isPositionValid(test_position))
+
+class TestPositionValid(TestCase):
+    def test_position_valid(self):
+        ARBITRARY_X = 4
+        ARBITRARY_Y = 7
+        test_map = Map()
+        test_position = Position(ARBITRARY_X, ARBITRARY_Y)
+        self.assertTrue(test_map.isPositionValid(test_position))
+
+class TestPositionInvalid(TestCase):
+    def test_position_invalid(self):
+        ARBITRARY_X = 10
+        ARBITRARY_Y = 7
+        test_map = Map()
+        test_position = Position(ARBITRARY_X, ARBITRARY_Y)
+        self.assertFalse(test_map.isPositionValid(test_position))
